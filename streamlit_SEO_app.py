91591dcd1dba381316f114cec0870e49f54b4b39
W11P <137043352+W11P@users.noreply.github.com>
2023-06-19 14:24:41 +0100

    Update streamlit_SEO_app.py

--- streamlit_SEO_app.py
+++ streamlit_SEO_app.py
@@ -1,7 +1,7 @@
 import re
 import requests
 from urllib.parse import urlparse, urljoin
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import colorama
 from collections import Counter
 from string import punctuation
